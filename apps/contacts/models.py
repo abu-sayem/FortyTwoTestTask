fda8e0aef9a8f2f06230b16ae299e12e4050857e
abu-sayem <abusaayem@gmail.com>
2021-02-27 14:45:22 +0600

    t1_add final contact model

--- apps/contacts/models.py
+++ apps/contacts/models.py
@@ -2,8 +2,20 @@ from django.db import models
 
 
 class Contact(models.Model):
-    """ Contact model containg user information """
-    pass
-    
-    def __str__(self) -> str:
-        pass
+    """ COntact model containg user information """
+
+    name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    contacts = models.TextField(blank=True)
+    email = models.EmailField(blank=True)
+    jabber = models.EmailField(blank=True)
+    skype = models.CharField(max_length=50, blank=True)
+    bio = models.TextField(blank=True)
+    other_contacts = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = 'contacts'
+
+    def __str__(self):
+        return self.name + ' ' + self.last_name
