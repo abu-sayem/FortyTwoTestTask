fde1b9b9294d8cb786b3473660724fe9f69fd175
abu-sayem <abusaayem@gmail.com>
2021-02-27 14:36:06 +0600

    t1_add test for models

--- apps/contacts/tests.py
+++ apps/contacts/tests.py
@@ -1,5 +1,36 @@
 from django.test import TestCase
 
+from apps.contacts.models import Contact
+
+
+class InfoModelTest(TestCase):
+    def setUp(self):
+        Contact.objects.create(
+            name='Abu',
+            last_name='Sayem',
+            date_of_birth='1993-10-25',
+            contacts='+8801750721112',
+            email='abusaayem@gmail.com',
+            jabber='abusayem.42cc.co',
+            skype='abusaayem',
+            bio='This is sample bio',
+            other_contacts='other contact info',
+        )
+        self.info = Contact.objects.get(id=1)
+
+    def test_string_representation(self):
+        """Test string representaions of info model"""
+        expected_info = f'{self.info.name} {self.info.last_name}'
+        self.assertEqual(expected_info, str(self.info))
+
+    def test_verbose_name_plural(self):
+        """Ckeeck verbose name"""
+        self.assertEqual(str(Contact._meta.verbose_name_plural), 'infos')
+
+    def test_info_exists(self):
+        """Cheeck existing data of info model after insertion"""
+        self.assertNotEqual(Contact.objects.count(), 0)
+
 
 class ContactViewTest(TestCase):
     def setUp(self):
