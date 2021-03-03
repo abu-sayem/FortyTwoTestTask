19c43856c8029a3a47eb3bf1852e419e91aa1e6b
abu-sayem <abusaayem@gmail.com>
2021-03-03 19:21:07 +0600

    t1_change default views to templateview

--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import get_object_or_404, render
-from django.views import View
+from django.views.generic.base import TemplateView
 from apps.contacts.models import Contact
 
 
-class ContactView(View):
+class ContactView(TemplateView):
     template_name = 'contacts/contact.html'
 
-    def get(self, request, *args, **kwargs):
-        data = get_object_or_404(Contact, pk=1)
-        return render(request, self.template_name, {'data': data})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['data'] = get_object_or_404(Contact, pk=1)
+        return context
