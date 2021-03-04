6975bea732f796cf50709ff3b022c03090dbd0f0
abu-sayem <abusaayem@gmail.com>
2021-03-04 12:00:47 +0600

    t3_add views use model and handle ajax request

--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import get_object_or_404, render
-from django.views.generic.base import TemplateView
 from apps.contacts.models import Contact, Log
 from django.views import View
 from django.http import JsonResponse
+from django.core.serializers import serialize
 
 
-class ContactView(TemplateView):
+class ContactView(View):
     template_name = 'contacts/contact.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['data'] = get_object_or_404(Contact, pk=1)
-        return context
+    def get(self, request, *args, **kwargs):
+        data = get_object_or_404(Contact, pk=1)
+        count = Log.objects.all().count()
+        if request.is_ajax():
+            context = {
+                'data': serialize('json', [data]),
+                'count': count,
+            }
+            return JsonResponse(context, safe=False)
+        return render(request, self.template_name, {'data': data, 'count': count})
 
 
 class LogView(View):
@@ -20,11 +26,9 @@ class LogView(View):
     def get(self, request, *args, **kwargs):
         data = Log.objects.all().order_by('-id')[:10].values()
         if request.is_ajax():
-            count = Log.objects.all().count()
             data_list = list(data)
             context = {
                 'data': data_list,
-                'count': count,
             }
             return JsonResponse(context, safe=False)
         return render(request, self.template_name, {'data': data})
