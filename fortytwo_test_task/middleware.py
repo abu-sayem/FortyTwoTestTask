df499563727b14adf139aa86185a14c2c851c0de
abu-sayem <abusaayem@gmail.com>
2021-03-04 12:01:45 +0600

    t3_change middleware exclude ajax request from log

--- fortytwo_test_task/middleware.py
+++ fortytwo_test_task/middleware.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from django.utils.deprecation import MiddlewareMixin
 
 from apps.contacts.models import Log
@@ -6,10 +5,10 @@ from apps.contacts.models import Log
 
 class RequestLoggingMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        log = Log()
-        log.path = request.path
-        log.method = request.method
-        log.user = request.user
-        log.time = datetime.now()
-        log.browser = request.META.get('HTTP_USER_AGENT')
-        log.save()
+        if not request.is_ajax():
+            log = Log()
+            log.path = request.path
+            log.method = request.method
+            log.user = request.user
+            log.browser = request.META.get('HTTP_USER_AGENT')
+            log.save()
