fe17f756ffe84b92bd9d5590ff968abe5bba4aa3
abu-sayem <abusaayem@gmail.com>
2021-03-06 00:36:22 +0600

    t3_remove ajax cheeck from middleware

--- fortytwo_test_task/middleware.py
+++ fortytwo_test_task/middleware.py
@@ -5,10 +5,9 @@ from apps.contacts.models import Log
 
 class RequestLoggingMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        if not request.is_ajax():
-            log = Log()
-            log.path = request.path
-            log.method = request.method
-            log.user = request.user
-            log.browser = request.META.get('HTTP_USER_AGENT', '')
-            log.save()
+        log = Log()
+        log.path = request.path
+        log.method = request.method
+        log.user = request.user
+        log.browser = request.META.get('HTTP_USER_AGENT', '')
+        log.save()
