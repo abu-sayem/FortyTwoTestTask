1d9854290cd861da229fd16cb6f81657937ff980
abu-sayem <abusaayem@gmail.com>
2021-03-08 03:27:39 +0600

    t5_ added login url

--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.ContactView.as_view()),
     path('log', views.LogView.as_view(), name='log'),
+    path('edit', views.EditView.as_view(), name='edit'),
+    path('login', auth_views.LoginView.as_view(template_name="contacts/login.html"), name="login"),
 ]
