de3d89b355ca5afe96756ab2c1f623c7626ae9c7
abu-sayem <abusaayem@gmail.com>
2021-03-09 22:28:29 +0600

    t3_add ajax_view

--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from apps.contacts import views
 
 urlpatterns = [
     path('', views.ContactView.as_view()),
-    path('log', views.LogView.as_view(), name='log'),
+    path('log', views.LogListView.as_view(), name='log'),
+    path('ajax', views.AJAXView.as_view(), name='ajax_view')
 ]
