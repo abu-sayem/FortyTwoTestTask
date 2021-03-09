096be6894e175afbb008f1b08c891d0757e4d245
abu-sayem <abusaayem@gmail.com>
2021-03-09 22:56:26 +0600

    t3_add tailing comma

--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -4,5 +4,5 @@ from apps.contacts import views
 urlpatterns = [
     path('', views.ContactView.as_view()),
     path('log', views.LogListView.as_view(), name='log'),
-    path('ajax', views.AJAXView.as_view(), name='ajax_view')
+    path('ajax', views.AJAXView.as_view(), name='ajax_view'),
 ]
