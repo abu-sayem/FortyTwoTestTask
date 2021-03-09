4af09718b9f5b62a8860c1f95b2a7eafc2aa6fbe
abu-sayem <abusaayem@gmail.com>
2021-03-09 22:29:49 +0600

    t3_remove cors header

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.contacts',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'fortytwo_test_task.middleware.RequestLoggingMiddleware',
 ]
 
